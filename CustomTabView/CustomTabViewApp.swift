c69f77d0a840c7979352cc0ac254b2a8aa8dab27
avi7501 <avinashmuralidharan2@gmail.com>
2023-09-15 15:35:29 +0530

    Initial Commit

---
+++ CustomTabView/CustomTabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabViewApp.swift
+//  CustomTabView
+//
+//  Created by Avinash Muralidharan on 15/09/23.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
